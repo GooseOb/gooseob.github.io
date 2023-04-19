da70fab8e4c969b99309b6feebe476b21d674cc4
GooseOb <GooseOb@mail.ru>
2023-04-19 15:27:57 +0200

    change 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 const NotFound: NextPage = () => {
 	return (
-		<div className='h-full flex flex-col items-center mt-40 text-5xl font-medium'>
+		<div className='h-full flex flex-col items-center mt-20 md:mt-40 text-5xl text-center font-medium'>
 			<Image src='/icon.svg' alt='logo' className='w-[4em] mb-[1em]' />
 			<div>404</div>
-			<div className='mb-[1em]'>Page not found</div>
+			<div className='mb-[1em]'>Goose hasn’t created this page yet</div>
 			<Link href='/'>Home page</Link>
 		</div>
 	);
