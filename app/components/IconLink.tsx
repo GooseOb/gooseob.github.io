b4992daf8b5ac61e93fadd54e5965a164f94802e
GooseOb <GooseOb@mail.ru>
2023-04-19 13:20:33 +0200

    fix links & add images to git

--- app/components/IconLink.tsx
+++ app/components/IconLink.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import internal from 'stream';
 
 type IconLinkProps = {
 	href: string;
 	src: string;
 	alt: string;
+	internal?: boolean;
 	as?: string;
 	className?: string;
 	linkClassName?: string;
@@ -19,23 +21,35 @@ const IconLink: FC<IconLinkProps> = ({
 	as = href,
 	src,
 	alt,
+	internal = false,
 	className = 'mx-3',
 	linkClassName = 'opacity-70 hover:opacity-100 select-none',
 	imgClassName = 'h-[1em] w-[1em]',
 	width = 100,
 	height = width!
-}) => (
-	<div className={className}>
-		<Link className={linkClassName} href={href} as={as}>
-			<Image
-				src={src}
-				className={imgClassName}
-				width={width}
-				height={height}
-				alt={alt}
-			/>
-		</Link>
-	</div>
-);
+}) => {
+	const img = (
+		<Image
+			src={src}
+			className={imgClassName}
+			width={width}
+			height={height}
+			alt={alt}
+		/>
+	);
+	return (
+		<div className={className}>
+			{internal ? (
+				<Link className={linkClassName} href={href} as={as}>
+					{img}
+				</Link>
+			) : (
+				<a className={linkClassName} href={href}>
+					{img}
+				</a>
+			)}
+		</div>
+	);
+};
 
 export default IconLink;
